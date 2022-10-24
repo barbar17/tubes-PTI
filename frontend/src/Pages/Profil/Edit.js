54a39f7d3cccd752c3472bc4fcf9d5ae63fa0b3a
barbar17 <akbarmaulana644@gmail.com>
2022-10-25 05:59:11 +0700

    create pengajuan dan profile

---
+++ frontend/src/Pages/Profil/Edit.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { IoArrowBackSharp } from "react-icons/io5";
+import { BsUpload } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
+
+function Profil() {
+  return (
+    <div className="min-h-screen w-full bg-slate-200 font-display">
+      <div className="min-h-screen flex flex-col justify-between">
+        <div className="container flex flex-col grow mx-auto my-10 border border-gray-400 shadow-2xl">
+          <div className="w-full flex bg-main h-20 items-center text-white text-5xl px-8">
+            <Link to={"/"}>
+              <IoArrowBackSharp />
+            </Link>
+            <span className="px-8">Biodata Pegawai</span>
+          </div>
+          <div className="flex relative">
+            <div className="absolute right-16 mt-16">
+              <button className="my-auto text-white bg-main h-12 w-36 items-center justify-center text-3xl rounded-lg">Simpan</button>
+            </div>
+            <div className="container flex flex-col w-[200px] place-items-center">
+              <div className="flex border border-gray-400 shadow-2xl w-[200px] h-[200px] object-cover object-center mt-20 ml-32 ">
+                <button className="my-auto text-white bg-main h-12 w-36 items-center justify-center text-2xl rounded-lg ml-7">
+                  <BsUpload className="mt-2 ml-6" />
+                  <span className="flex ml-14 -mt-6">Foto</span>
+                </button>
+              </div>
+              <div className="flex border border-gray-400 shadow-2xl w-[200px] h-[200px] object-cover object-center mt-20 ml-32 ">
+                <button className="my-auto text-white bg-main h-12 w-36 items-center justify-center text-2xl rounded-lg ml-7">
+                  <BsUpload className="mt-2 ml-6" />
+                  <span className="flex ml-14 -mt-6">TTD</span>
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col ml-64 my-auto mt-40 text-3xl space-y-4">
+              <div>
+                Nama&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;: <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+              <div>
+                NIP/ID&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp; : <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+              <div>
+                Tempat/Tgl Lahir&emsp;: <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+              <div>
+                Divisi&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;: <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+              <div>
+                Agama&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp; &nbsp;: <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+              <div>
+                Alamat&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp; : <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+              <div>
+                Telepon&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;: <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+              <div>
+                E-mail&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;: <input type="text" className="rounded-lg border-gray-600" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Profil;
--- frontend/src/Pages/Profil/Profil.js
+++ frontend/src/Pages/Profil/Profil.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { IoArrowBackSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import Footer from '../Footer/Footer';
 
 function Profil() {
     return (
-        <div>awdaws</div>
+        <div className="min-h-screen w-full bg-slate-200 font-display">
+            <div className="min-h-screen flex flex-col justify-between">
+                <div className="container flex flex-col grow mx-auto my-10 border border-gray-400 shadow-2xl">
+                    <div className="w-full flex bg-main h-20 items-center text-white text-5xl px-8">
+                        <Link to={"/"}>
+                            <IoArrowBackSharp />
+                        </Link>
+                        <span className="px-8">Biodata Pegawai</span>
+                    </div>
+                    <div className="flex relative">
+                        <div className="absolute right-16 mt-16">
+                            <button className="my-auto text-white bg-emerald-400 h-12 w-24 items-center justify-center text-3xl rounded-lg">Edit</button>
+                        </div>
+                        <div className="container flex flex-col w-[200px]">
+                            <img src="/gambar/robi.jpeg" className="w-[200px] h-[200px] object-cover object-center mt-20 ml-32" />
+                            <img src="/gambar/ttd.jpeg" className="w-[200px] h-[125px] object-cover object-center mt-32 ml-32" />
+                        </div>
+                        <div className="flex flex-col ml-64 my-auto mt-40 text-3xl space-y-4">
+                            <div>Nama&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;: Widodo</div>
+                            <div>NIP/ID&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp; : 12345</div>
+                            <div>Tempat/Tgl Lahir&emsp;: Sukarame, 05-04-1000 SM</div>
+                            <div>Divisi&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;: Produksi</div>
+                            <div>Agama&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp; &nbsp;: Islam</div>
+                            <div>Alamat&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp; : Jn. In Dulu Aja No.1, Sukarame, Bandar Lampung</div>
+                            <div>Telepon&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;: 081234567890</div>
+                            <div>E-mail&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;: widodotitikdua@gmail.com</div>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </div>
     )
 }
 
