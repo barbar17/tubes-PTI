dd92e1bf38dc0a8355b9630bb3496bf4930b5904
SalsabillaPutri <dyanisalsabilla@gmail.com>
2022-10-29 14:30:01 +0700

    menu

---
+++ frontend/src/Pages/DataPegawai/DataPegawaiAdmin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DataPegawaiAdmin() {
+  return <div>DataPegawaiAdmin</div>;
+}
+
+export default DataPegawaiAdmin;
--- frontend/src/Pages/Header/Menu.js
+++ frontend/src/Pages/Header/Menu.js
@@ -1,25 +1,30 @@
-
 export function Menus() {
-    const link = [
-        {
-            title: 'Beranda',
-            path: '/user/beranda',
-            active: "h-full w-[250px] px-5 text-white bg-active-btn flex items-center justify-center",
-            notActive: "h-full w-[250px] px-5 text-white flex items-center justify-center hover:bg-active-btn transition-colors"
-        },
-        {
-            title: 'Pengajuan E-Cuti',
-            path: '/user/pengajuan_e-cuti',
-            active: "h-full w-[250px] px-5 text-white bg-active-btn flex items-center justify-center",
-            notActive: "h-full w-[250px] px-5 text-white flex items-center justify-center hover:bg-active-btn transition-colors"
-        },
-        {
-            title: 'Informasi Masa Cuti',
-            path: '/user/informasi_masa_cuti',
-            active: "h-full w-[250px] px-5 text-white bg-active-btn flex items-center justify-center",
-            notActive: "h-full w-[250px] px-5 text-white flex items-center justify-center hover:bg-active-btn transition-colors"
-        }
-    ]
+  const link = [
+    {
+      title: "Beranda",
+      path: "/user/beranda",
+      active:
+        "h-full w-[250px] px-5 text-white bg-active-btn flex items-center justify-center",
+      notActive:
+        "h-full w-[250px] px-5 text-white flex items-center justify-center hover:bg-active-btn transition-colors",
+    },
+    {
+      title: "Pengajuan E-Cuti",
+      path: "/user/pengajuan_e-cuti",
+      active:
+        "h-full w-[250px] px-5 text-white bg-active-btn flex items-center justify-center",
+      notActive:
+        "h-full w-[250px] px-5 text-white flex items-center justify-center hover:bg-active-btn transition-colors",
+    },
+    {
+      title: "Informasi Masa Cuti",
+      path: "/user/informasi_masa_cuti",
+      active:
+        "h-full w-[250px] px-5 text-white bg-active-btn flex items-center justify-center",
+      notActive:
+        "h-full w-[250px] px-5 text-white flex items-center justify-center hover:bg-active-btn transition-colors",
+    },
+  ];
 
-    return link
+  return link;
 }
