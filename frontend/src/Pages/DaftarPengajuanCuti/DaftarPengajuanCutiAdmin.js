3dd3a110083adcb0f20b00d0ccdead27c78848c8
barbar17 <akbarmaulana644@gmail.com>
2022-10-31 07:14:26 +0700

    fixed beberapa pages

--- frontend/src/Pages/DaftarPengajuanCuti/DaftarPengajuanCutiAdmin.js
+++ frontend/src/Pages/DaftarPengajuanCuti/DaftarPengajuanCutiAdmin.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import { BiSearchAlt } from 'react-icons/bi'
+import React from "react";
+import KonfirmasiAdmin from "./KonfirmasiAdmin";
+import { useState } from "react";
 
 function DaftarPengajuanCutiAdmin() {
+    const [buttonKonfirmAdm, setButtonKonfirmAdm] = useState(false);
     return (
-        <div className="w-full p-10 h-full">
-            <div className="w-full">
-                <div className="bg-main w-full text-white py-2 px-5 text-2xl rounded-tl-lg rounded-tr-lg">Daftar Pengajuan Cuti</div>
-                <div className='flex flex-col p-5 bg-white'>
-                    <div className='relative w-80 mb-5' >
-                        <input
-                            type={'text'}
-                            className="w-full h-10 bg-slate-100 outline outline-2 outline-slate-400 rounded-md pl-14 pr-10 text-sm focus:shadow-slate-400 focus:shadow-md transition-all"
-                            placeholder='Ketik untuk mencari... '
-                        />
-                        <div className="absolute top-1/2 -translate-y-1/2 left-2 pr-1 border-r-2 h-full border-r-slate-400 flex items-center">
-                            <BiSearchAlt className='text-2xl' color='black' />
-                        </div>
-                    </div>
-                    <table className="table-auto w-full text-center text-xl ">
+        <div className="flex flex-col w-full">
+            <div className="flex justify-around w-full">
+                <div className="absolute w-4/5 -mt-64 ml-[900px]">
+                    <KonfirmasiAdmin
+                        trigger={buttonKonfirmAdm}
+                        setTrigger={setButtonKonfirmAdm}
+                    />
+                </div>
+            </div>
+
+            <div className="w-full p-10 h-full">
+                <div className="w-full">
+                    <div className="bg-main w-full text-white py-2 px-5 text-2xl rounded-tl-lg rounded-tr-lg">Data Pegawai</div>
+                    <table className="table-auto w-full text-center bg-white text-xl ">
                         <thead>
                             <tr className="border-b-2">
                                 <th className="py-2">#</th>
@@ -25,7 +26,7 @@ function DaftarPengajuanCutiAdmin() {
                                 <th>Nama</th>
                                 <th>Tgl Pengajuan</th>
                                 <th>Tgl Mulai</th>
-                                <th>Tgl Selesai</th>
+                                <th>Selesai</th>
                                 <th>Alasan</th>
                                 <th>Opsi</th>
                             </tr>
@@ -38,13 +39,13 @@ function DaftarPengajuanCutiAdmin() {
                                 <td>10/01/2022</td>
                                 <td>11/01/2022</td>
                                 <td>15/01/2022</td>
-                                <td>Sakit</td>
+                                <td>sakit</td>
                                 <td>
                                     <div className="w-full h-full flex justify-evenly">
                                         <button className="my-auto text-white bg-card-green h-8 w-24 items-center justify-center text-lg rounded-lg">
                                             Profil
                                         </button>
-                                        <button className="my-auto text-white bg-card-red h-8 w-24 items-center justify-center text-lg rounded-lg">
+                                        <button onClick={() => setButtonKonfirmAdm(true)} className="my-auto text-white bg-btn-purple h-8 w-24 items-center justify-center text-lg rounded-lg">
                                             Detail
                                         </button>
                                     </div>
@@ -55,7 +56,7 @@ function DaftarPengajuanCutiAdmin() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default DaftarPengajuanCutiAdmin
+export default DaftarPengajuanCutiAdmin;
---
+++ frontend/src/Pages/DataPegawai/EditAkunPegawai.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+
+function EditAkunPegawai() {
+    const [nama, setNama] = useState("");
+    const [nip, setNip] = useState("");
+    const [divisi, setDivisi] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className="flex flex-col w-full items-center justify-center py-10 px-20 gap-20">
+            <div className="w-full">
+                <div className="bg-main w-full text-white py-2 px-5 text-2xl rounded-tl-lg rounded-tr-lg text-center">
+                    Edit Akun Pegawai
+                </div>
+                <div className=" w-full flex items-center justify-center bg-white text-xl p-10">
+                    <table className="border-separate border-spacing-y-2 border-spacing-x-5">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <span>Nama</span>
+                                </td>
+                                <td>
+                                    :{" "}
+                                    <input
+                                        type={"text"}
+                                        className="rounded-lg border-slate-400 border "
+                                        placeholder="Rangga Tangguh "
+                                        value={nama}
+                                        onChange={(event) => setNama(event.target.value)}
+                                        required
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>NIP/ID</span>
+                                </td>
+                                <td>
+                                    :{" "}
+                                    <input
+                                        type="text"
+                                        className="rounded-lg border-slate-400 border"
+                                        placeholder="12345 "
+                                        value={nip}
+                                        onChange={(event) => setNip(event.target.value)}
+                                        required
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>Divisi</span>
+                                </td>
+                                <td>
+                                    :{" "}
+                                    <input
+                                        type="text"
+                                        className="rounded-lg border-slate-400 border"
+                                        placeholder="Produksi "
+                                        value={divisi}
+                                        onChange={(event) => setDivisi(event.target.value)}
+                                        required
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>Username</span>
+                                </td>
+                                <td>
+                                    :{" "}
+                                    <input
+                                        type="text"
+                                        className="rounded-lg border-slate-400 border"
+                                        placeholder="Widodo2000 "
+                                        value={username}
+                                        onChange={(event) => setUsername(event.target.value)}
+                                        required
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>Password</span>
+                                </td>
+                                <td>
+                                    :{" "}
+                                    <input
+                                        type="text"
+                                        className="rounded-lg border-slate-400 border"
+                                        placeholder="passwordku123 "
+                                        value={password}
+                                        onChange={(event) => setPassword(event.target.value)}
+                                        required
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>{""}</td>
+                                <div className="flex justify-end w-full pr-1">
+                                    <button className="my-auto text-white bg-indigo-500 h-8 w-24 items-center justify-center text-lg rounded-lg">
+                                        Submit
+                                    </button>
+                                </div>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <hr className="border-gray-400 border-2 my-5" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EditAkunPegawai;
